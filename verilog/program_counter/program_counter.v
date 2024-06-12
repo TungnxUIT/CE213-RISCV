b09b46e13f81738f10100483f39ec45afeff796f
Tung <100463571+TungnxUIT@users.noreply.github.com>
2024-06-12 14:20:55 +0700

    Update program_counter.v

--- verilog/program_counter/program_counter.v
+++ verilog/program_counter/program_counter.v
@@ -4,19 +4,15 @@ input clk, reset;
 input [31:0] pc_next;
 output reg[31:0] pc, pc_4;
 
-initial begin
-	pc = 0;
-	pc_4 = pc + 4;
-end
 
 always @(posedge clk or posedge reset) begin
   if (reset) begin
-    pc = -4;  // Set pc to 0 when reset is asserted
-	 pc_4 = 0;
+    pc <= -4;  // Set pc to 0 when reset is asserted
+	 pc_4 <= 0;
   end else begin
-    pc = pc_next;
+    pc <= pc_next;
   end
-	pc_4 = pc + 4;	//wait for updating pc before updating pc_4
+	pc_4 <= pc + 4;	//wait for updating pc before updating pc_4
 end
 
 endmodule
